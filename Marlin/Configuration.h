3ba1acf199360d8bac5145ca2a5753113ce822ca
ViktorFran <47641564+ViktorFran@users.noreply.github.com>
2020-10-16 11:01:47 -0400

    Adjusting endstop direction

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -650,13 +650,13 @@
 #endif
 
 // Mechanical endstop with COM to ground and NC to Signal uses "false" here (most common setup).
-#define X_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define Y_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
+#define X_MIN_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
+#define Y_MIN_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
 #define Z_MIN_ENDSTOP_INVERTING true //false //Inverted for 3pin switch // Set to true to invert the logic of the endstop.
 #define X_MAX_ENDSTOP_INVERTING true //false //Inverted for 3pin switch // Set to true to invert the logic of the endstop.
 #define Y_MAX_ENDSTOP_INVERTING true //false //Inverted for 3pin switch // Set to true to invert the logic of the endstop.
-#define Z_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define Z_MIN_PROBE_ENDSTOP_INVERTING false // Set to true to invert the logic of the probe.
+#define Z_MAX_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
+#define Z_MIN_PROBE_ENDSTOP_INVERTING true // Set to true to invert the logic of the probe.
 
 /**
  * Stepper Drivers
@@ -1112,8 +1112,8 @@
 
 // Direction of endstops when homing; 1=MAX, -1=MIN
 // :[-1,1]
-#define X_HOME_DIR 1
-#define Y_HOME_DIR 1
+#define X_HOME_DIR -1
+#define Y_HOME_DIR -1
 #define Z_HOME_DIR -1
 
 // @section machine
